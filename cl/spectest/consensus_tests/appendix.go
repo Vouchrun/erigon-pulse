687e1b9b15b4eee9740a1a75f50269b755253b38
kewei <kewei.train@gmail.com>
2026-07-15 16:02:20 +0700

    cl/ssz: retain attestation fork version for hashing

--- cl/spectest/consensus_tests/appendix.go
+++ cl/spectest/consensus_tests/appendix.go
@@ -214,7 +214,9 @@ func addSszTests() {
 		)).
 		With("Attestation", sszStaticTestNewObjectByFunc(
 			func(v clparams.StateVersion) *solid.Attestation {
-				return &solid.Attestation{}
+				attestation := &solid.Attestation{}
+				attestation.SetVersion(v)
+				return attestation
 			}, withTestJson(),
 		)).
 		With("SyncCommitteeMessage", sszStaticTestByEmptyObject(&cltypes.SyncCommitteeMessage{}, withTestJson())).
